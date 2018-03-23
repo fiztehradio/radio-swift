0f1b4499181286b4084064cdb7aaef89a5376034
Aleks Bykhun <theisease@gmail.com>
2018-03-23 16:17:59 +0300

    Initial Commit

---
+++ fiztehradio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fiztehradio
+//
+//  Created by Aleksey Bykhun on 23.03.2018.
+//  Copyright © 2018 caffeinum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
